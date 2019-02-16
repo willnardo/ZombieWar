9d3b3af9c9fcbca12c9a30dcfb42a99fc476b665
xionga13 <46729226+xionga13@users.noreply.github.com>
2019-02-15 20:54:48 -0600

    Add files via upload
    Uploaded the Weapons class again with the extended classes.

---
+++ Shotgun.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewarteam2;
+
+/**
+ *
+ * @author Aladdin
+ * 100 CSC 422 Software Engineering
+ */
+public class Shotgun extends Weapon{
+    
+    //Constructor
+    public Shotgun(String name){
+        //Not sure how to do the types yet if it is just the same as name
+        super.setName(name);
+        super.setDamage(30);
+        super.setAccuracy(1/5);
+    }
+}
